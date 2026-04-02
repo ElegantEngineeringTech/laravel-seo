fd1dbb54977c879f43711715b7ed73b01fb39efc
Quentin Gabriele <quentin.gabriele@gmail.com>
2026-04-02 14:18:30 +0200

    pint

--- src/helpers.php
+++ src/helpers.php
@@ -9,11 +9,11 @@ if (! function_exists('seo')) {
     function seo(null|HasSeo|SeoManager $value = null): SeoManager
     {
         if ($value === null) {
-            return \Elegantly\Seo\Facades\SeoManager::current();
+            return Elegantly\Seo\Facades\SeoManager::current();
         }
 
         if ($value instanceof HasSeo) {
-            return \Elegantly\Seo\Facades\SeoManager::current()->apply($value);
+            return Elegantly\Seo\Facades\SeoManager::current()->apply($value);
         }
 
         return $value;
